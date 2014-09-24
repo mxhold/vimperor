57853065e23a14f8291aca6af315a2f60d259930
Max Holder <mxhold@gmail.com>
2014-09-24 15:03:25 -0400

    Refactor hashid
    This removes duplication and makes it clearer that 6 is the minimum length

--- app/services/hashid.rb
+++ app/services/hashid.rb
@@ -1,9 +1,24 @@
 class Hashid
   def self.encode(id)
-    Hashids.new(Rails.application.secrets.hashid_salt, 6).encode(id)
+    hashids.encode(id)
   end
 
   def self.decode(hashid)
-    Hashids.new(Rails.application.secrets.hashid_salt, 6).decode(hashid).first
+    hashids.decode(hashid).first
   end
+
+  def self.hashids
+    @hashids ||= Hashids.new(salt, minimum_length)
+  end
+  private_class_method :hashids
+
+  def self.salt
+    Rails.application.secrets.hashid_salt
+  end
+  private_class_method :salt
+
+  def self.minimum_length
+    6
+  end
+  private_class_method :minimum_length
 end
