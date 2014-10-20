009a92aa49411c257d30eda04acdeea964009b83
Max Holder <mxhold@gmail.com>
2014-10-19 21:00:41 -0400

    WIP: OptionSetPresenter refactor

--- app/presenters/option_set_presenter.rb
+++ app/presenters/option_set_presenter.rb
@@ -13,32 +13,65 @@ class OptionSetPresenter
 
   private
 
-  def option(*keys)
-    keys.reduce(@options) do |memo, key|
-      memo.fetch(key.to_s, nil) if memo.respond_to?(:fetch)
+  def renderer_options
+    deep_convert_to_hash convert(@options)
+  end
+
+  def deep_convert_to_hash(array)
+    array.map do |key, value|
+      if value && value.respond_to?(:to_h) && value.to_h != value
+        [key.to_sym, deep_convert_to_hash(value)]
+      else
+        [key.to_sym, value]
+      end
+    end.to_h
+  end
+
+  def convert(options, option_group: nil)
+    options.map do |option, value|
+      if value.respond_to?(:fetch)
+        [option, convert(value, option_group: option)]
+      else
+        if option_group
+          [option, coerce_to_type(value, option_types[option_group.to_sym][option.to_sym])]
+        else
+          [option, coerce_to_type(value, option_types[option.to_sym])]
+        end
+      end
+    end
+  end
+
+  def coerce_to_type(value, type)
+    case type
+    when :boolean
+      value == 'true' || value == '1'
+    when :string
+      value unless value.empty?
+    when :integer
+      value.to_i
     end
   end
 
   # rubocop:disable Metrics/MethodLength
-  def renderer_options
+  def option_types
     {
-      compatible: to_boolean(option(:compatible)),
-      leader: option(:leader),
+      compatible: :boolean,
+      leader: :string,
       backspace: {
-        indent: to_boolean(option(:backspace, :indent)),
-        eol: to_boolean(option(:backspace, :eol)),
-        start: to_boolean(option(:backspace, :start))
+        indent: :boolean,
+        eol: :boolean,
+        start: :boolean,
       },
-      expandtab: to_boolean(option(:expandtab)),
-      tab_width: option(:tab_width),
+      expandtab: :boolean,
+      tab_width: :integer,
       list: {
-        list: to_boolean(option(:list, :list)),
-        trail: nil_if_blank(option(:list, :trail)),
-        tab: nil_if_blank(option(:list, :tab)),
-        eol: nil_if_blank(option(:list, :eol)),
-        nbsp: nil_if_blank(option(:list, :nbsp)),
-        extends: nil_if_blank(option(:list, :extends)),
-        precedes: nil_if_blank(option(:list, :precedes)),
+        list: :boolean,
+        trail: :string,
+        tab: :string,
+        eol: :string,
+        nbsp: :string,
+        extends: :string,
+        precedes: :string,
       }
     }
   end
