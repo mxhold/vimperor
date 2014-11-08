f5f3a958a79c28a610dcf94fd8af0432e8f20b8c
Max Holder <mxhold@gmail.com>
2014-11-08 10:28:42 -0500

    Extract config path to constant

--- lib/options.rb
+++ lib/options.rb
@@ -4,12 +4,14 @@ require_relative 'options/registry'
 require_relative 'options/option_registry'
 
 module Options
+  CONFIG_PATH = "#{__dir__}/options/config.yml"
+
   def self.find(name)
     registry.find(name)
   end
 
   def self.registry
-    @registry ||= OptionRegistry.new("#{__dir__}/options/config.yml")
+    @registry ||= OptionRegistry.new(CONFIG_PATH)
   end
   private_class_method :registry
 
