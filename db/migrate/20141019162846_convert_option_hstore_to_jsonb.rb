5d7ba5f3f9397ad766aa103d95e57b2bcad0db76
Max Holder <mxhold@gmail.com>
2014-11-08 14:47:49 -0500

    Fix migrations for Heroku
    I wouldn’t normally edit migrations but Heroku cannot
    get passed the migration that added the jsonb type since it
    is not running on Postgres 9.4.

--- db/migrate/20141019162846_convert_option_hstore_to_jsonb.rb
+++ db/migrate/20141019162846_convert_option_hstore_to_jsonb.rb
@@ -1,7 +1,8 @@
 class ConvertOptionHstoreToJsonb < ActiveRecord::Migration
   def change
     remove_column :option_sets, :options, :hstore
-    add_column :option_sets, :options, :jsonb
+    # add_column :option_sets, :options, :jsonb
+    add_column :option_sets, :options, :json
     disable_extension :hstore
   end
 end
--- db/migrate/20141019193445_change_jsonb_to_json.rb
+++
@@ -1,6 +0,0 @@
-class ChangeJsonbToJson < ActiveRecord::Migration
-  def change
-    remove_column :option_sets, :options, :jsonb
-    add_column :option_sets, :options, :json
-  end
-end
